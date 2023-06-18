70164648aca9be45e855ab025c5980e02800b826
ivan <ippeng54@gmail.com>
2023-06-18 22:21:43 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,105 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+# Для начала определим настройки запуска
+hostName = "localhost" # Адрес для доступа по сети
+serverPort = 8080 # Порт для доступа по сети
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
+    def __get_html_content(self):
+        return """
+        <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Меню</h5>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Главная</li>
+                    <li class="list-group-item">Категории</li>
+                    <li class="list-group-item">Заказы</li>
+                    <li class="list-group-item">Контанты</li>
+                </ul>
+                <div class="card-body">
+                    <h8 class="card-title">Пользователь</h8>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="row mt-5" style="text-align: center">
+                <h2 class="card-title">Контакты</h2>
+            </div>
+            <div class="row mt-5">
+                <div class="col-6">
+                    <div class="card">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Имя</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Почта</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlTextarea1" class="form-label">Сообщение</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <button type="button" class="btn btn-primary">Отправить</button>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Наши контакты</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
+        """
+
+
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        page_content = self.__get_html_content()
+        self.send_response(200) # Отправка кода ответа
+        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
+        self.end_headers() # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(page_content, "utf-8")) # Тело ответа
+
+if __name__ == "__main__":
+    # Инициализация веб-сервера, который будет по заданным параметрам в сети
+    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+        pass
+
+    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+    webServer.server_close()
+    print("Server stopped.")
